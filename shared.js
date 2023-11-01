7adcde42c2d68b40310fe725353be797196538ae
Jessica <jesica.jaramillo@gmail.com>
2023-11-01 08:20:07 -0500

    probar evento comprar facebook 2

--- shared.js
+++ shared.js
@@ -1,11 +1,16 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal-pag');
 let selectLeeFragmento = document.querySelector('.read');
+let selectComprar = document.querySelector('##comprarButton')
 let selectCloseButton = document.querySelector('.close');
 
 selectLeeFragmento.addEventListener('click', function() {
     backdrop.style.display = "block";
     modal.style.display = "grid";
+});
+
+selectComprar.addEventListener('click', function() {
+    fbq('track', 'Purchase', {currency: "USD", value: 4.99});
 });
 
 backdrop.addEventListener('click', closeModal);
